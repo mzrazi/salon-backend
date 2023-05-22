aff32a973c631f95fa9fb0409cbfa294327f511f
Muhammed zaeem razi <mzrazi7@gmail.com>
2023-05-22 15:07:43 +0530

    changes

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -1,14 +1,17 @@
+const servicemodel = require("../models/servicemodel");
+
 module.exports={
-    calculateCurrentPrice:(service)=> {
-        const offer = service.offer;
+    calculateCurrentPrice:async(service)=> {
+       const selectedservice=await servicemodel.findById(service._id).populate('offer').exec()
+       const offer =selectedservice.offer
       
         if (offer) {
           const discountPercentage = offer.discountPercentage;
-           const result=service.price - (service.price * discountPercentage / 100);
+           const result=selectedservice.price - (selectedservice.price * discountPercentage / 100);
            console.log(result);
            return result
         } else {
-          return service.price;
+          return selectedservice;
         }
       }
 }
